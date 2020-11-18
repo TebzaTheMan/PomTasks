4c8efe1d3f9965e49cd4fbdac63e1c520b44e71b
Tebogo <tebzax2@gmail.com>
2020-11-18 18:51:25 +0200

    timer countsdown

--- src/components/TabPanel.js
+++ src/components/TabPanel.js
@@ -4,9 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
 import TabView from './TabView';
 
 function TabPanel(props) {
--- src/components/header.js
+++ src/components/header.js
@@ -29,6 +29,7 @@ export default function Header() {
           <Typography variant="h6" className={classes.title}>
             Pomodoro Timer
           </Typography>
+          
           <IconButton edge="end" className={classes.menuButton} color="inherit" aria-label="menu" >
           <HelpIcon/>
           </IconButton>
