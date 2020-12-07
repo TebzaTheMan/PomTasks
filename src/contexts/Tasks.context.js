22dcb590097027c273263b3c0696c9e79f63ddf9
Tebogo <tebzax2@gmail.com>
2020-12-07 20:10:00 +0200

    FEATURE:persisted tasks into local storage

--- src/contexts/Tasks.context.js
+++ src/contexts/Tasks.context.js
@@ -1,12 +1,27 @@
 import React, { createContext, useReducer } from "react";
 import tasksReducer from "./../reducers/Tasks.reducer";
+import useLocalStorageReducer from "../hooks/useLocalStorageReducer";
 const TasksContext = createContext();
 export const DispatchContext = createContext();
 export default TasksContext;
 
-const defaultTasks = [];
+const defaultTasks = [
+  {
+    id: 1,
+    task: "Welcome to Pomodoro Timer",
+    pomodorosDone: "0",
+    pomodorosEstimated: "1",
+    notes: "Use this app to boost your productivity",
+    isDone: false,
+    isDoing: false,
+  },
+];
 export function TasksProvider(props) {
-  const [tasks, dispatch] = useReducer(tasksReducer, defaultTasks);
+  const [tasks, dispatch] = useLocalStorageReducer(
+    "tasks",
+    tasksReducer,
+    defaultTasks
+  );
   return (
     <TasksContext.Provider value={tasks}>
       <DispatchContext.Provider value={dispatch}>
