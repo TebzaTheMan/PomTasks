b92062f97024837cd9d3965160f10f72c27d4635
Tebogo <tebzax2@gmail.com>
2020-12-21 20:04:38 +0200

    style : change name of app color to white

--- src/components/header.js
+++ src/components/header.js
@@ -21,7 +21,7 @@ export default function Header() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6" className={classes.title} color="inherit">
             Pomodoro Timer
           </Typography>
           <Report />
