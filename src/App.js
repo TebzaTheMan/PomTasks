96d0ae9366367da3eba43d3d318fb30a3af54dbb
Tebogo <tebzax2@gmail.com>
2020-12-21 18:21:15 +0200

    feat : used theme Provider to change theme of the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Header from './components/header';
 import TabPanel from './components/TabPanel';
 import Tasks from './components/Tasks/Tasks';
@@ -7,17 +8,32 @@ import { TasksProvider } from './contexts/Tasks.context';
 import { WeeklyDataProvider } from './contexts/WeeklyData.context';
 import InfoBar from './components/Tasks/InfoBar';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: '#B45CC3',
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#11cb5f',
+    },
+  },
+});
+
 function App() {
   return (
     <div className="App">
-      <TasksProvider>
-        <WeeklyDataProvider>
-          <Header />
-          <TabPanel />
-        </WeeklyDataProvider>
-        <Tasks />
-        <InfoBar />
-      </TasksProvider>
+      <ThemeProvider theme={theme}>
+        <TasksProvider>
+          <WeeklyDataProvider>
+            <Header />
+            <TabPanel />
+          </WeeklyDataProvider>
+          <Tasks />
+          <InfoBar />
+        </TasksProvider>
+      </ThemeProvider>
     </div>
   );
 }
