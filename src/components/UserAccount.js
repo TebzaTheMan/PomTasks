89a46fafff740d3ac5b444375b1618fa04e1c838
Tebogo <tebzax2@gmail.com>
2020-12-24 11:33:25 +0200

    feat : able to login and logout and display your profile picture if logged in.

--- src/components/UserAccount.js
+++ src/components/UserAccount.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
-import { signInWithGoogle } from '../firebase';
+import Avatar from '@material-ui/core/Avatar';
+import { signInWithGoogle, auth } from '../firebase';
+import { UserContext } from '../contexts/User.context';
 
 export default function UserAccount() {
+  const user = useContext(UserContext);
+  const { photoURL, displayName } = user || {};
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
 
@@ -16,6 +20,9 @@ export default function UserAccount() {
   const handleClose = () => {
     setAnchorEl(null);
   };
+  const signOut = () => {
+    auth.signOut();
+  };
   return (
     <div>
       <IconButton
@@ -25,25 +32,46 @@ export default function UserAccount() {
         onClick={handleMenu}
         color="inherit"
       >
-        <AccountCircle />
+        {user ? <Avatar alt={displayName} src={photoURL} /> : <AccountCircle />}
       </IconButton>
-      <Menu
-        id="menu-appbar"
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        keepMounted
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        open={open}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={signInWithGoogle}>Login with Google</MenuItem>
-      </Menu>
+      { user ? (
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={signOut}>logout</MenuItem>
+        </Menu>
+      ) : (
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={signInWithGoogle}>Login with Google</MenuItem>
+        </Menu>
+      )}
+
     </div>
   );
 }
