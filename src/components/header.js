6587480bc060727f07e04a0e7c2a0bd4bb261cdf
Tebogo <tebzax2@gmail.com>
2020-12-24 11:07:04 +0200

    chore : moved sign in button to separate component

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
 import Report from './Report/Report';
-import { signInWithGoogle } from '../firebase';
+import UserAccount from './UserAccount';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -21,16 +17,6 @@ const useStyles = makeStyles(() => ({
 
 export default function Header() {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   return (
     <div className={classes.root}>
       <AppBar position="static">
@@ -39,34 +25,7 @@ export default function Header() {
             Pomodoro Timer
           </Typography>
           <Report />
-          <div>
-            <IconButton
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={open}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={signInWithGoogle}>Login with Google</MenuItem>
-            </Menu>
-          </div>
+          <UserAccount />
         </Toolbar>
       </AppBar>
     </div>
