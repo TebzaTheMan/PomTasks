93fbde9e6cbd790ceb7817348ff65328f5efe796
Tebogo <tebzax2@gmail.com>
2020-12-31 15:43:18 +0200

    feat : add email and github buttons

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import EmailIcon from '@material-ui/icons/Email';
+import IconButton from '@material-ui/core/IconButton';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import Report from './Report/Report';
 
 const useStyles = makeStyles(() => ({
@@ -25,6 +28,26 @@ export default function Header() {
             PomTasks
           </Typography>
           <Report />
+          <IconButton
+            aria-label="close report-modal"
+            aria-controls="close-modal"
+            edge="end"
+            color="inherit"
+            // eslint-disable-next-line no-restricted-globals
+            onClick={() => { location.href = 'https://github.com/tebzatheman/pomtasks'; }}
+          >
+            <GitHubIcon />
+          </IconButton>
+          <IconButton
+            aria-label="close report-modal"
+            aria-controls="close-modal"
+            color="inherit"
+            edge="end"
+            // eslint-disable-next-line no-restricted-globals
+            onClick={() => { location.href = 'mailto:tebzax2@gmail.com?subject="Feedback about PomTasks"&body="write some message"'; }}
+          >
+            <EmailIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </div>
